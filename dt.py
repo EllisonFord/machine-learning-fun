32822449ace59186e5fe22f54518260114513eb8
Jay <moka7452@gmail.com>
2019-04-01 01:20:42 +0200

    Finally got this index thing working

--- dt.py
+++ dt.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from math import log2
+from math import log10
 
 
 def pr(items_in_class, sum_items_all_classes):
     return items_in_class / sum_items_all_classes
 
 
-def i_error(prob):
-    return 1-prob
+def i_error(data):
+    total = sum(data)
+    highest_prob = 0.0
+    for i in data:
+        if pr(i, total) > highest_prob:
+            highest_prob = pr(i, total)
+    return 1 - highest_prob
 
 
 def diff_i(s, t):
@@ -18,29 +23,34 @@ def diff_i(s, t):
 
 def i_entropy(t):
     i_h = 0
-    tot = len(t)
+    total = sum(t)
     for i in t:
         if i is 0:
             continue
-        i_h -= pr(i, tot)*log2(pr(i, tot))
-    return i_h
+        i_h += pr(i, total)*log10(pr(i, total))
+    return -i_h
 
 
-def i_gini(t):
+def i_gini(data):
     s = 0
-    tot = len(t)
-    for i in t:
-        s += pr(i, tot)
+    total = sum(data)
+    for i in data:
+        s += pr(i, total)**2
     return 1 - s
 
 
 def main():
 
-    tee = range(10)
+    data = range(10)
 
-    print(i_entropy(tee))
+    data = (10, 30)
 
-    print(i_gini(tee))
+    print('Error:', i_error(data))
+
+    print('Entropy:', i_entropy(data))
+
+    print('Gini index:', i_gini(data))
+
 
 
 if __name__ == '__main__':
