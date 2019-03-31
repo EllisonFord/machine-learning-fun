1078d0d73746f0d931050148876f2792c8644161
MASTERPC\Administrator <hosein.mirzai@tum.de>
2019-03-31 13:43:51 +0200

    Tensorflow tests.

--- tensorflow_web.py
+++ tensorflow_web.py
@@ -1,13 +1,64 @@
 #!/usr/bin/env python3
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
-model = keras.Sequential()
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+
+print(train_labels)
+
+plt.figure()
+
+plt.imshow(train_images[0])
+
+plt.colorbar()
+plt.gca().grid(False)
+
+#plt.pause(0)
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 
-# Adds a densely-connected layer with 64 units to the model:
-model.add(keras.layers.Dense(64, activation='relu'))
-# Add another:
-model.add(keras.layers.Dense(64, activation='relu'))
-# Add a softmax layer with 10 output units:
-model.add(keras.layers.Dense(10, activation='softmax'))
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+
+    plt.xlabel(class_names[train_labels[i]])
+
+#plt.pause(0)
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+
+predictions = model.predict(test_images)
+
+
+
