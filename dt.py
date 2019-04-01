7190df75c92e9b2029b31211b96553d90238bd1d
Jay <moka7452@gmail.com>
2019-04-01 02:28:33 +0200

    Something or other.

--- dt.py
+++ dt.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-
-import numpy as np
 from math import log10
+import numpy as np
 
 
-def pr(items_in_class, sum_items_all_classes):
-    return items_in_class / sum_items_all_classes
+def pr(i, s):
+    return i / s
 
 
-def i_error(data):
+def information_gain(i_main_branch, i_left, i_right, pr_left, pr_right):
+    return i_main_branch - i_left*pr_left - i_right*pr_right
+
+
+def i_error(data):  # Information Classification Error
     total = sum(data)
     highest_prob = 0.0
     for i in data:
@@ -17,23 +20,19 @@ def i_error(data):
     return 1 - highest_prob
 
 
-def diff_i(s, t):
-    return i_error(t) - pr(0, 1)*i_error(0.5) - pr(0, 1)*i_error(0.5)
-
-
 def i_entropy(t):
-    i_h = 0
     total = sum(t)
+    i_h = 0.0
     for i in t:
-        if i is 0:
+        if i is 0:  # Special case
             continue
         i_h += pr(i, total)*log10(pr(i, total))
     return -i_h
 
 
 def i_gini(data):
-    s = 0
     total = sum(data)
+    s = 0.0
     for i in data:
         s += pr(i, total)**2
     return 1 - s
@@ -41,16 +40,27 @@ def i_gini(data):
 
 def main():
 
-    data = range(10)
+    data_m = (40, 40)
+    print('Error:', i_error(data_m))
+    print('Entropy:', i_entropy(data_m))
+    print('Gini index:', i_gini(data_m))
+    print()
 
-    data = (10, 30)
+    data_l = (30, 10)
+    print('Error:', i_error(data_l))
+    print('Entropy:', i_entropy(data_l))
+    print('Gini index:', i_gini(data_l))
+    print()
 
-    print('Error:', i_error(data))
+    data_r = (10, 30)
+    print('Error:', i_error(data_r))
+    print('Entropy:', i_entropy(data_r))
+    print('Gini index:', i_gini(data_r))
+    print()
 
-    print('Entropy:', i_entropy(data))
-
-    print('Gini index:', i_gini(data))
-
+    print('IG Error:', information_gain(i_error(data_m), i_error(data_l), i_error(data_r), 0.5, 0.5))
+    print('IG Gini:', information_gain(i_gini(data_m), i_gini(data_l), i_gini(data_r), 0.5, 0.5))
+    print('IG Entropy:', information_gain(i_entropy(data_m), i_entropy(data_l), i_entropy(data_r), 0.5, 0.5))
 
 
 if __name__ == '__main__':
