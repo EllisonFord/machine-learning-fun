8e9ad5c65399638b0487e1f6d89633a711061da0
MASTERPC\Administrator <hosein.mirzai@tum.de>
2019-04-05 15:04:40 +0200

    Some plots

--- linear_classification.py
+++ linear_classification.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import matplotlib.pyplot as plt
 from math import exp
 
 
@@ -9,13 +10,40 @@ def softmax(predictions):
     return [exp(x) / denominator for x in predictions]
 
 
-def main():
+def hyperplane(w, w0, N=range(100)):
+    samples = []
+    for x in N:
+        samples.append(w*x+w0)
+    return samples
+
+
+def main(*args, **kwargs):
 
     predictions = [1, 2, 2, 4, 1]
 
     sm_predictions = softmax(predictions)
 
     print(sm_predictions)
+
+    sample_range = range(5)
+
+    plot1 = hyperplane(1, 1, sample_range)
+    plot2 = hyperplane(2, 2, sample_range)
+    plot3 = hyperplane(3, 3, sample_range)
+    plot4 = hyperplane(0, 0, sample_range)
+    plot5 = hyperplane(-1, -1, sample_range)
+    plot6 = hyperplane(-2, -2, sample_range)
+    plot7 = hyperplane(-3, -3, sample_range)
+
+    plt.plot(sample_range, plot1)
+    plt.plot(sample_range, plot2)
+    plt.plot(sample_range, plot3)
+    plt.plot(sample_range, plot4)
+    plt.plot(sample_range, plot5)
+    plt.plot(sample_range, plot6)
+    plt.plot(sample_range, plot7)
+    plt.show()
+
 
 
 if __name__ == '__main__':
