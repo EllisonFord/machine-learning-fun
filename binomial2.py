fb9cf8bb7de14a47ad83e9df9db7d9f784202683
Jay <moka7452@gmail.com>
2019-09-01 00:54:08 +0200

    Noice

--- binomial2.py
+++ binomial2.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 from math import factorial as fac
 import matplotlib.pyplot as plt
+from math import sqrt
+
+
+def mean(n, p):
+    return n*p
+
+
+def variance(n, p):
+    return n*p*(1-p)
+
+
+def std_dev(var):
+    return sqrt(var)
 
 
 def binomial(total, selection):
@@ -11,20 +24,29 @@ def binomial(total, selection):
     return binom
 
 
-def p(pr, total, selection):
-    return binomial(total, selection)*(pr**selection) * (1-pr)**(total-selection)
+def pr(p, total, selection):
+    return binomial(total, selection)*(p**selection) * (1-p)**(total-selection)
 
 
-maximum = 25
-minimum = 5
+n = 20
+p = 0.82
 
-print(p(pr=0.82, total=20, selection=18))
+
+maximum = 21
+minimum = 18
+
+#print(pr(p=p, total=n, selection=18))
 
 results = []
 for i in range(minimum, maximum):
-    results.append(p(pr=0.82, total=20, selection=i))
+    results.append(pr(p=p, total=n, selection=i))
+
 print(sum(results))
 
 plt.bar(range(minimum, maximum), results)
 
 plt.show()
+
+print(mean(n, p))
+
+print(variance(n, p))
