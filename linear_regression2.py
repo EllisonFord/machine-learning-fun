b279d8bd72a2bf376792cc7d5aecaf5bae933e70
Jay <moka7452@gmail.com>
2019-09-03 01:52:51 +0200

    Such a nice script

--- linear_regression2.py
+++ linear_regression2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def mean(v):
-    return sum(v)/len(v)
+    return sum(v) / len(v)
 
 
 def b_1(x, y):
@@ -11,13 +11,13 @@ def b_1(x, y):
     my = mean(y)
     numerator = denominator = 0.
     for i in range(len(x)):
-        numerator += (x[i]-mx)*(y[i]-my)
-        denominator += (x[i]-mx)**2
+        numerator += (x[i] - mx) * (y[i] - my)
+        denominator += (x[i] - mx) ** 2
     return numerator / denominator
 
 
-def b_0(x, y, b_1):
-    return (sum(y) - (b_1*sum(x))) / len(x)
+def b_0(x, y, b1):
+    return (sum(y) - (b1 * sum(x))) / len(x)
 
 
 def get_b0_b1(x, y):
@@ -30,14 +30,13 @@ xv = (65, 65, 62, 67, 69, 65, 61, 67)
 
 yv = (105, 125, 110, 120, 140, 135, 95, 130)
 
-#plt.scatter(x=xv, y=yv)
-#plt.show()
+# plt.scatter(x=xv, y=yv)
+# plt.show()
 
 b, m = get_b0_b1(xv, yv)
 
 plt.plot(xv, yv, '.')
-#plt.plot(m, b, '-')
+# plt.plot(m, b, '-')
 plt.show()
 
 print(get_b0_b1(xv, yv))
-
