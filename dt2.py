04f7c04d7e60476cbb044b886a90954a3a9d53e2
Jay <moka7452@gmail.com>
2019-09-07 00:34:13 +0200

    pandas not accessing columns? what is this ransaquery.

--- dt2.py
+++ dt2.py
@@ -1,29 +1,18 @@
 #!/usr/bin/env python3
+
 from math import log, exp, e
-import tabulate
 import numpy as np
-from numpy import genfromtxt
-
-my_data = genfromtxt('homework_01_dataset.csv', delimiter=',')
+import pandas
 
 
-type(my_data)
+def main():
+
+    r = pandas.read_csv('homework_01_dataset.csv', header=0)
+
+    # print(r['z'])
+
+    print(r)
 
 
-with open('homework_01_dataset.csv', mode='r') as f:
-
-    line_count = 0
-
-    #print(tabulate.tabulate(csv_reader, headers=['x1', 'x2', 'x3', 'z'], tablefmt="orgtbl"))
-
-    """
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {",".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]}\t{row[1]}\t{row[2]}')
-            line_count += 1
-    print(f'\nProcessed {line_count} lines.')
-
-    """
+if __name__ == '__main__':
+    main()
