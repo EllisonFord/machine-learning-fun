7aaaad8dcc96db5f69bf323c8940c9ac1bfc6249
Jay <moka7452@gmail.com>
2020-02-02 13:58:32 +0100

    best

--- convexity.py
+++ convexity.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,31 +15,41 @@ def f(x):
     return -x*x*x
 
 
-def test_set(x, y, P):
-    for s in range(P+1):
-        res = (1-s/P)*x + (s/P)*y
+def f_2(x):
+    return -x*x*x + 2*x*x + 5
+
+
+def test_set(x, y, p):
+    for s in range(p+1):
+        res = (1-s/p)*x + (s/p)*y
         print(res)
 
 
-def test_func(x, y, P):
-    for s in range(P+1):
-        lam = s/P
+def test_func(x, y, p):
+    for s in range(p+1):
+        lam = s/p
         lhs = lam*f(x) + (1-lam)*f(y)
         rhs = f(lam*x + (1-lam)*y)
         check(lhs, rhs)
 
 
-def plot_it(x, y, P):
+def plot_it(x, y, p):
     y_pts = []
     for s in range(x, y+1):
         y_pts.append(-1)
 
 
-P = 50
+def main(*args, **kwargs):
 
-x = 15
+    p = 50
 
-y = -15
+    x = 15
 
-test_func(x, y, P)
-plot_it(x, y, P)
+    y = -15
+
+    test_func(x, y, p)
+    plot_it(x, y, p)
+
+
+if __name__ == '__main__':
+    main()
--- optimisation.py
+++ optimisation.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def main(*args, **kwargs):
+def momentum(grads: list, lr, gamma=0.5):
+    m_t = 0.
+    weight = 0.
+
+    for grad in grads:
+        m_t = lr*grad+gamma*m_t
+        print('m_t:', m_t)
+        weight = weight - m_t
+        print('weight:', weight)
+
+
+def adam():
     pass
+
+
+def main(*args, **kwargs):
+    momentum([5, 7, 3, 1], lr=1)
 
 
 if __name__ == '__main__':
