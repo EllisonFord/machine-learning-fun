f01da63b9e7f6e403cbb66c76bf4f35acec07a5b
Jay <moka7452@gmail.com>
2020-02-07 23:51:01 +0100

    Better matrices

--- svd.py
+++ svd.py
@@ -3,33 +3,39 @@
 import numpy as np
 
 
-U, SIG, V = range(3)
-
 A = np.array([[7, 2.5],
               [2, 0]])
 
 
-SVD = np.linalg.svd(A)
+U, SIG, V = np.linalg.svd(A)
+
+print(U)
 
 print('\nU')
-print(SVD[U])
+print(U)
 
 print('\nSIGMA')
-print(SVD[SIG])
+print(SIG)
 
 print('\nV')
-print(SVD[V])
+print(V)
 
 print('\nPutting things back?')
-R = SVD[U] @ SVD[SIG] @ SVD[V].T
+R = U @ SIG @ V.T
 print(R)
 
 print('\nProjected:')
-P = SVD[U] @ SVD[SIG]
+P = U @ SIG
 print(P)
 
 
 summand = 0.
-for row in SVD[U]:
+for row in U:
     summand += row[0]**2
+print(np.sqrt(summand))
+
+
+for row in V:
+    summand = sum(row**2)
+    break
 print(np.sqrt(summand))
