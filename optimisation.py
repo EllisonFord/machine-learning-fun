200ac91703fca82652a1cfc957df422da3f22b4f
claxiux <hosein.mirzai@tum.de>
2022-06-14 11:37:51 +0200

    Calculations for gravity added

--- optimisation.py
+++ optimisation.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def gravitational_force(mass_1, mass_2, d):
+    g = 6.67408e-11
+    return (g*mass_1*mass_2)/(d**2)
+
+
+def gravity(mass, d):
+    g = 6.67408e-11
+    return (g*mass)/(d**2)
+
+
 def momentum(grads: list, lr, gamma=0.5):
     m_t = 0.
     weight = 0.
@@ -15,12 +25,17 @@ def momentum(grads: list, lr, gamma=0.5):
         print('weight:', weight)
 
 
-def adam():
-    pass
-
-
 def main(*args, **kwargs):
-    momentum([5, 7, 3, 1], lr=1)
+
+    earth_mass = 5.972e24
+    earth_radius = 6.371e6
+    r = gravity(earth_mass, earth_radius)
+    print(round(r, 4), 'm/s')
+
+    r = gravitational_force(earth_mass, 80, earth_radius)
+    print(round(r, 4), 'N')
+
+    # momentum([5, 7, 3, 1], lr=1)
 
 
 if __name__ == '__main__':
